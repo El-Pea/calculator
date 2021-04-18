1c92ad42de70f7f17caaaea102345dc65af58b07
El-Pea <62609661+El-Pea@users.noreply.github.com>
2021-04-18 15:57:54 -0400

    structuring

---
+++ app.js
@@ -0,0 +1,31 @@
+/*
+needed functions:
+add 
+subtract
+divide
+multiply
+toggle +/-
+
+operate (takes operator, two numbers, and calls the operator on them)
+*/
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function posNeg(a){
+    return a.sign === 1 ? a.sign === -1 : a.sign === 1;
+    // needs work 
+}
