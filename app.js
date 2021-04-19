fe6b64df80af74b65f8a8868ffe9937581384c8f
El-Pea <62609661+El-Pea@users.noreply.github.com>
2021-04-19 16:53:14 -0400

    finished operator()

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/*
-needed functions:
-add 
-subtract
-divide
-multiply
-toggle +/-
-
-operate (takes operator, two numbers, and calls the operator on them)
-*/
-
 function add(a, b){
     return a + b;
 }
@@ -29,10 +18,7 @@ function posNeg(a){
     return a > 0 ? a * -1: Math.abs(a);
 }
 
-function operate(a, b, operator){
+function operate(operator, a, b){
     if(operator === posNeg){return operator(a)}
     return operator(a, b);
-    // works except posNeg
-    // will rest parameters help me out here?
-    // will putting operator first help me out here?
 }
