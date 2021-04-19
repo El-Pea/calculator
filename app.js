2282c9deb1f26c834d749094c72169c32c57a96c
El-Pea <62609661+El-Pea@users.noreply.github.com>
2021-04-19 16:45:14 -0400

    css exp

--- app.js
+++ app.js
@@ -28,3 +28,11 @@ function divide(a, b){
 function posNeg(a){
     return a > 0 ? a * -1: Math.abs(a);
 }
+
+function operate(a, b, operator){
+    if(operator === posNeg){return operator(a)}
+    return operator(a, b);
+    // works except posNeg
+    // will rest parameters help me out here?
+    // will putting operator first help me out here?
+}
