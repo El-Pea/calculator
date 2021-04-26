49e87e8f0e0335e6828eac7ef99d6765c0723fd5
El-Pea <62609661+El-Pea@users.noreply.github.com>
2021-04-26 17:09:59 -0400

    click listeners

--- app.js
+++ app.js
@@ -22,3 +22,33 @@ function operate(operator, a, b){
     if(operator === posNeg){return operator(a)}
     return operator(a, b);
 }
+
+let display = {
+    one : [],
+    two : [],
+};
+
+function updateScreen(){
+    const number = document.querySelectorAll('.num__num');
+    const displayDiv = document.querySelector('.calc__display');
+    number.forEach((num)=>{
+        num.addEventListener('click', ()=>{
+            displayDiv.textContent = num.textContent;
+        });
+    });
+}
+
+/*
+function displayThis(){
+    const number = document.querySelectorAll('.num__num');
+    const display = document.querySelector('.calc__display');
+    number.forEach((num)=>{
+        num.addEventListener('click', ()=>{
+            display.textContent = num.textContent;
+            console.log(num.textContent)
+        });
+    });
+}
+*/
+
+updateScreen();
